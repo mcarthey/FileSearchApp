eaec8ce326d30a3f42d3b13d6737e961a93a9844
Mark McArthey <mcarthey@gmail.com>
2024-06-08 18:31:49 -0500

    trying to get elasticsearch responsive on the backend docker container

--- file-search-app/src/app/file-search.service.ts
+++ file-search-app/src/app/file-search.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FileSearchService {
-  private apiUrl = 'http://localhost:5000/api/filesearch';  // Ensure this URL is correct
+  private apiUrl = 'http://file_search_api:5000/api/filesearch';  // Use service name instead of localhost
 
   constructor(private http: HttpClient) { }
 
